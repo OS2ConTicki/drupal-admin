825d40ac625c78ce5e0a90b1396f49f8e967486c
Mikkel Ricky <rimi@aarhus.dk>
2020-07-11 10:43:51 +0200

    Added organizer

---
+++ web/modules/custom/conference_fixtures/src/Fixture/OrganizerFixture.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\conference_fixtures\Fixture;
+
+use Drupal\content_fixtures\Fixture\AbstractFixture;
+use Drupal\content_fixtures\Fixture\DependentFixtureInterface;
+use Drupal\node\Entity\Node;
+
+/**
+ * Class OrganizerFixture.
+ *
+ * @package Drupal\conference_fixtures\Fixture
+ */
+class OrganizerFixture extends AbstractFixture implements DependentFixtureInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function load() {
+    /** @var \Drupal\node\Entity\Node $organizer */
+    $organizer = Node::create([
+      'type' => 'organizer',
+      'title' => 'Someone',
+      'field_conference' => $this->getReference('conference:001'),
+    ]);
+    $organizer->setOwner($this->getReference('user:organizer'));
+
+    $this->setReference('organizer:someone', $organizer);
+
+    $organizer->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDependencies() {
+    return [
+      ConferenceFixture::class,
+      UserFixture::class,
+    ];
+  }
+
+}
