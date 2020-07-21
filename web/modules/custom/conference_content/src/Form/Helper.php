a7367c24f4e4da2394a0375e630e8d579694071e
Mikkel Ricky <rimi@aarhus.dk>
2020-07-21 10:25:31 +0200

    Added conference entities block

--- web/modules/custom/conference_content/src/Form/Helper.php
+++ web/modules/custom/conference_content/src/Form/Helper.php
@@ -88,36 +88,7 @@ class Helper {
     $formState->set(['conference_content', 'conference'], $conference);
 
     $weight = 10000;
-    foreach ([
-      'event' => [
-        'title' => $this->t('Events'),
-        'add' => $this->t('Add event'),
-      ],
-      'speaker' => [
-        'title' => $this->t('Speakers'),
-        'add' => $this->t('Add speaker'),
-      ],
-      'tag' => [
-        'title' => $this->t('Tags'),
-        'add' => $this->t('Add tag'),
-      ],
-      'location' => [
-        'title' => $this->t('Locations'),
-        'add' => $this->t('Add location'),
-      ],
-      'theme' => [
-        'title' => $this->t('Themes'),
-        'add' => $this->t('Add theme'),
-      ],
-      'sponsor' => [
-        'title' => $this->t('Sponsors'),
-        'add' => $this->t('Add sponsor'),
-      ],
-      'organizer' => [
-        'title' => $this->t('Organizers'),
-        'add' => $this->t('Add organizer'),
-      ],
-    ] as $type => $info) {
+    foreach ($this->conferenceHelper->getConferenceEntititesInfo() as $type => $info) {
       $entities = $this->conferenceHelper->getEntitites($conference, $type);
 
       $form['conference_' . $type] = [
--- web/modules/custom/conference_content/src/Helper/ConferenceHelper.php
+++ web/modules/custom/conference_content/src/Helper/ConferenceHelper.php
@@ -3,6 +3,8 @@
 namespace Drupal\conference_content\Helper;
 
 use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\node\Entity\Node;
 use Drupal\node\NodeInterface;
 
@@ -10,6 +12,7 @@ use Drupal\node\NodeInterface;
  * Conference helper.
  */
 class ConferenceHelper {
+  use StringTranslationTrait;
 
   /**
    * The entity repository.
@@ -21,8 +24,48 @@ class ConferenceHelper {
   /**
    * Constructor.
    */
-  public function __construct(EntityRepositoryInterface $entityRepository) {
+  public function __construct(EntityRepositoryInterface $entityRepository, TranslationInterface $translation) {
     $this->entityRepository = $entityRepository;
+    $this->setStringTranslation($translation);
+  }
+
+  /**
+   * Get info on entities that belong to a conference.
+   *
+   * @return array[]
+   *   Info on entities.
+   */
+  public function getConferenceEntititesInfo() {
+    return [
+      'event' => [
+        'title' => $this->t('Events'),
+        'add' => $this->t('Add event'),
+      ],
+      'speaker' => [
+        'title' => $this->t('Speakers'),
+        'add' => $this->t('Add speaker'),
+      ],
+      'tag' => [
+        'title' => $this->t('Tags'),
+        'add' => $this->t('Add tag'),
+      ],
+      'location' => [
+        'title' => $this->t('Locations'),
+        'add' => $this->t('Add location'),
+      ],
+      'theme' => [
+        'title' => $this->t('Themes'),
+        'add' => $this->t('Add theme'),
+      ],
+      'sponsor' => [
+        'title' => $this->t('Sponsors'),
+        'add' => $this->t('Add sponsor'),
+      ],
+      'organizer' => [
+        'title' => $this->t('Organizers'),
+        'add' => $this->t('Add organizer'),
+      ],
+    ];
   }
 
   /**
